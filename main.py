67c15e2ccaed09c73c61eca8c9be2151a1d442be
AdrianBigBalls <apoverdijk@gmail.com>
2020-10-06 16:43:42 +0200

    Separated functions from main

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# Eindopdracht test test okay
-print("lulkoek")
+# Eindopdracht Adrian
+import menus
+
+print("**** Reminder List ****")
+userName = input("please enter your username:")
+
+if userName == "":
+    print("Username not entered, exiting the program...")
+
+print("Welcome " + userName + ", please make a choice in the menu:")
+
+menus.print_menu()
---
+++ menus.py
@@ -0,0 +1,39 @@
+myList = []
+
+def print_menu():
+    print(" 1 - Print list\n 2 - Add to list\n 3 - Delete from list\n 4 - Quit")
+    choice = int(input())
+    if choice == 1:
+        print_list()
+    elif choice == 2:
+        add_item()
+    elif choice == 3:
+        delete_item()
+    elif choice == 4:
+        exit()
+    else:
+        print("Not a valid choice, exitting...")
+        exit()
+
+def print_list():
+    for i in myList:
+        print("* ", i)
+    choice = input("Press M to enter the menu")
+    if choice == "m" or choice == "M":
+        print_menu()
+
+
+def add_item():
+    print("**** Add Item ****\n")
+    item = input("Enter an item to add to your list: ")
+    myList.append(item)
+    print("You added " + item + " to your list")
+    print_menu()
+
+def delete_item():
+    print("**** Delete Item ****\n")
+    for (i, x) in enumerate(myList):
+        print(i, x)
+    item = int(input("Select an index number to delete: "))
+    myList.pop(item)
+    print_menu()
