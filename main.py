78ccadf1e781c8608feffac1e398e1d899fef268
AdrianBigBalls <apoverdijk@gmail.com>
2020-10-07 18:39:09 +0200

    exit when no username is entered

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print("**** Reminder List ****")
 userName = input("please enter your username:")
 
 if userName == "":
-    print("Username not entered, exiting the program...")
+    exit("Username not entered, exiting the program...")
 
 print("Welcome " + userName + ", please make a choice in the menu:")
 
